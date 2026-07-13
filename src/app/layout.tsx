bb4f41d014da3d727a6a204ee63556a0984cc142
azrilpramudia <azrilpramudia01@gmail.com>
2026-07-14 00:07:32 +0700

    chore: change name title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import Navbar from "../components/layout/Navbar";
 import Footer from "../components/layout/Footer";
 
 export const metadata: Metadata = {
-  title: "meowhx.dev",
+  title: "meowhx",
   description: "Personal Website",
 };
 
